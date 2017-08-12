1546ad04efdabb5fe044f06e84d02d64a927adb6
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2017-08-12 16:55:39 -0600

    incluir carpeta de imagenes de categoria, comenzar estructura de pagina de categorias, mostrar usuario correspondiente o invitado dependiendo si ha iniciado sesion o no

---
+++ views/categorias_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Categorias</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<header>
+		<div class="bar">
+			<h1><?php echo $user ?></h1>	
+		</div>
+	</header>
+
+	<div class="contenedor_cat">
+		<div class="categoria">
+			<img src="images/categorias/lamp.png" alt="">
+			<h2>categoria</h2>
+		</div>
+	</div>
+
+</body>
+</html>

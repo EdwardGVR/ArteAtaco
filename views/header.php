77b9964d3d95680196c768b4affd5a16ec78b9bf
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2017-11-04 18:56:42 -0600

    Aplicar flexbox a la barra
    Hacer responsive la barra de opciones

--- views/header.php
+++ views/header.php
@@ -6,7 +6,7 @@
 
 	<div class="bar-options">
 		<div class="dropmenu bar-btn">
-			<a href="categorias.php"><h1 class="drop-btn categorias">categorias</h1></a>
+			<a href="categorias.php"><h1 class="drop-btn categorias">Categorias</h1></a>
 			<div class="drop-content">
 				<?php foreach ($categorias as $categoria): ?>
 					<a href="productos.php?id=<?php echo $categoria['id'] ?>"><?php echo $categoria['nombre_cat'] ?></a>
@@ -19,21 +19,23 @@
 			<a class="bar-btn">A cerca</a>
 	</div>
 
-	<div class="dropmenu">
-			<?php if (isset($_SESSION['user'])): ?>
-				<h1 class="user"><?php echo $user ?></h1>
-				<div class="drop-content">
-					<a href="#">Cuenta</a>
-					<a href="#">Pedidos</a>
-					<a href="logout.php">Cerrar Sesi&oacute;n</a>
-				</div>
-			<?php else: ?>
-				<h1 class="user"><?php echo $user ?> <i class="fa fa-user-plus"></i></h1>
-				<div class="drop-content">
-					<a href="login.php">Iniciar Sesi&oacute;n</a>
-					<a href="register.php">Registrarse</a>
-				</div>
-			<?php endif ?>
+	<div class="user">
+		<div class="dropmenu">
+				<?php if (isset($_SESSION['user'])): ?>
+					<h1 class="user-btn"><?php echo $user ?></h1>
+					<div class="drop-content">
+						<a href="#">Cuenta</a>
+						<a href="#">Pedidos</a>
+						<a href="logout.php">Cerrar Sesi&oacute;n</a>
+					</div>
+				<?php else: ?>
+					<h1 class="user-btn"><?php echo $user ?> <i class="fa fa-user-plus"></i></h1>
+					<div class="drop-content">
+						<a href="login.php">Iniciar Sesi&oacute;n</a>
+						<a href="register.php">Registrarse</a>
+					</div>
+				<?php endif ?>
+		</div>
 	</div>
 </div>
 

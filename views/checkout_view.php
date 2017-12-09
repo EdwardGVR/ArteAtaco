21bc0599e09cd8feeb4cf6284f77f2c7797a3d94
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2017-12-09 12:46:26 -0600

    Checkout
    Continuar maquetando pagina de checkout, agregar formulario de metodos de pago y boton de confirmar informacion.

--- views/checkout_view.php
+++ views/checkout_view.php
@@ -15,7 +15,8 @@
 	<div class="contenedor_checkout">
 		<div class="info_checkout">
 			<div class="contenedor_address">
-				<h3>Seleccione una direccion de envio</h3>
+				<div class="step1">1</div>
+				<h3 class="indication">Seleccione una direccion de envio</h3>
 				<div class="shipping_address">
 					Direccion de envio
 				</div>
@@ -28,8 +29,11 @@
 			</div>
 			<hr>
 			<div class="new_address">
-				<form class="form_new_address" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+				<form class="form_new_address" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+					<h3 class="indication">O agregue una nueva</h3>
 					<span class="add_address">Agregar una nueva direccion</span>
+					Nombre de la direccion:
+					<input type="text" name="address_line_1" class="new_address_field" placeholder="Nombre descriptivo ej: Casa Santa Ana, Oficina, etc..">
 					Pa&iacute;s:
 					<input type="text" name="pais" class="new_address_field" value="El Salvador" readonly>
 					Departamento:
@@ -51,8 +55,32 @@
 					<input type="text" name="address_line_1" class="new_address_field" placeholder="Direccion linea 1">
 					<input type="text" name="address_line_2" class="new_address_field" placeholder="Direccion linea 2">
 					Referencias:
-					<textarea name="referencias" id="ref" class="new_address_field" placeholder="referencias de ubicacion"></textarea>
+					<textarea name="referencias" id="ref" class="new_address_field" placeholder="Referencias de ubicacion ej: Frente a iglesia, a la par de local X, etc..."></textarea>
 					<input type="submit" name="add_address" value="Agregar direccion">
+				</form>
+			</div>
+			<hr>
+			<div class="payment_method">
+				<div class="step1">2</div>
+				<h3 class="indication">Seleccione un metodo de pago</h3>
+				<form class="form_pay_method" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+					<div class="pay_option">
+						<i class="fa fa-university" aria-hidden="true"></i><input type="radio" name="payment_method" value="bank-transfer">Transferencia bancaria
+					</div>	
+					<div class="pay_option">
+						<i class="fa fa-money" aria-hidden="true"></i><input type="radio" name="payment_method" value="method">Metodo de pago
+					</div>
+					<div class="pay_option">
+						<i class="fa fa-money" aria-hidden="true"></i><input type="radio" name="payment_method" value="method">Metodo de pago
+					</div>
+				</form>
+			</div>
+			<hr>
+			<div class="confirm_info">
+				<div class="step1">3</div>
+				<!-- <h3 class="indication">Confirmar informacion</h3> -->
+				<form class="form_confirm_info" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+					<input class="send_info" type="submit" name="confirm_info" value="Confirmar informacion">
 				</form>
 			</div>
 		</div>

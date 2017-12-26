70055bec342b8a88122034cb4e121957d34ed735
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2017-12-26 14:19:23 -0600

    Cambiar campo
    Cambiar columna numero por codigo en tabla pedidos

--- Database/arteataco.sql
+++ Database/arteataco.sql
@@ -140,7 +140,7 @@ DROP TABLE IF EXISTS `pedidos`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pedidos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `numero` int(11) NOT NULL,
+  `codigo` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `id_direccion` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
@@ -256,4 +256,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-24 16:47:02
+-- Dump completed on 2017-12-26 14:17:55

8e178c83ab98462ecfecdb4f239338bff429c228
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-01-25 16:02:28 -0600

    contacto
    El correo se envia correctamente desde el formulario de contacto, se uso la clase PHPMailer con el SMTP del hosting awardspace como host

---
+++ mensaje_sent.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!isset($_SESSION['user'])) {
+	session_start();
+}
+
+require 'functions.php';
+require 'conexion.php';
+
+if (isset($_SESSION['user'])) {
+	$user = $_SESSION['user'];
+	$iduser = get_user_id($conexion, $user);
+} else {
+	$user = 'Invitado';
+	$iduser = false;
+}
+
+if ($conexion != false) {
+	$query = $conexion->prepare('SELECT * FROM categorias ORDER BY nombre_cat ASC');
+	$query->execute();
+	$categorias = $query->fetchall();
+}
+
+require 'views/message_sent_view.php';
+
+?>

937cbeab300fd06a706632b1797fb1f8b109a5db
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-03-30 16:44:44 -0600

    Admin - inicio
    Maquetar pagina de inicio (con pedidos mas recientes)

---
+++ admin/views/index_view.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">   
+    <link rel="stylesheet" href="css/styles.css">
+    <title>ArteAteataco :: Admin</title>
+</head>
+<body>
+    <nav>
+        <div class="side_bar">
+            <div class="side_user_info"></div>
+            <div class="side_icons">
+                <div class="side_icon"><i class="fas fa-shopping-bag"></i></div>
+                <div class="side_icon"><i class="fas fa-cubes"></i></div>
+                <div class="side_icon"><i class="fas fa-users"></i></div>
+                <div class="side_icon"><i class="fas fa-truck"></i></div>
+            </div>
+        </div>
+        <div class="options">
+            <div class="user_info">
+                <div class="image">
+                    <img src="" alt="usrimg">
+                </div>
+                <div class="data">
+                    <span class="nombre">Nombre usuario</span>
+                    <hr>
+                    <span>Administrador</span>
+                </div>
+            </div>
+
+            <div class="buttons">
+                <a href="#">Pedidos</a>
+                <a href="#">Productos</a>
+                <a href="#">Clientes</a>
+                <a href="#">Puntos de entrega</a>
+            </div>
+        </div>
+    </nav>
+    <main>
+        <div class="bar">
+            <h1>Inicio</h1>
+        </div>
+        <section>
+            <div class="title">
+                <h2>Pedidos recientes</h2>
+                <hr>
+            </div>
+            <div class="contenedor_pedidos">
+                <article class="pedido">
+                    <div class="pedido_cliente">
+                        <span class="nombre">Nombre</span>
+                        <span class="apellido">Apellido</span>
+                        <div class="img_cliente"></div>
+                    </div>
+                    <hr>
+                    <div class="pedido_productos">
+                        <div class="producto">
+                            <span class="cantidad">1x</span>
+                            <span class="nombre_producto">Nombre del producto</span>
+                            <span class="precio">$00.00</span>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="pedido_direccion">
+                        <span class="departamento">Departamento</span>
+                        <span class="nombre_direccion">Nombre direccion</span>
+                        <div class="info_direccion tooltip"><i class="fas fa-info-circle"></i>
+                            <span class="tooltiptext">Detalles de la direccion de envio para este pedido</span>
+                        </div>
+                    </div>
+                    <div class="pedido_total">
+                        <span>$00.00</span>
+                    </div>
+                </article>
+                <article class="pedido">
+                    <div class="pedido_cliente">
+                        <span class="nombre">Nombre</span>
+                        <span class="apellido">Apellido</span>
+                        <div class="img_cliente"></div>
+                    </div>
+                    <hr>
+                    <div class="pedido_productos">
+                        <div class="producto">
+                            <span class="cantidad">1x</span>
+                            <span class="nombre_producto">Nombre del producto</span>
+                            <span class="precio">$00.00</span>
+                        </div>
+                        <div class="producto">
+                            <span class="cantidad">1x</span>
+                            <span class="nombre_producto">Nombre del producto</span>
+                            <span class="precio">$00.00</span>
+                        </div>
+                        <div class="producto">
+                            <span class="cantidad">1x</span>
+                            <span class="nombre_producto">Nombre del producto</span>
+                            <span class="precio">$00.00</span>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="pedido_direccion">
+                        <span class="departamento">Departamento</span>
+                        <span class="nombre_direccion">Nombre direccion</span>
+                        <div class="info_direccion tooltip"><i class="fas fa-info-circle"></i>
+                            <span class="tooltiptext">Detalles de la direccion de envio para este pedido</span>
+                        </div>
+                    </div>
+                    <div class="pedido_total">
+                        <span>$00.00</span>
+                    </div>
+                </article>
+                <article class="pedido">
+                    <div class="pedido_cliente">
+                        <span class="nombre">Nombre</span>
+                        <span class="apellido">Apellido</span>
+                        <div class="img_cliente"></div>
+                    </div>
+                    <hr>
+                    <div class="pedido_productos">
+                        <div class="producto">
+                            <span class="cantidad">1x</span>
+                            <span class="nombre_producto">Nombre del producto</span>
+                            <span class="precio">$00.00</span>
+                        </div>
+                        <div class="producto">
+                            <span class="cantidad">1x</span>
+                            <span class="nombre_producto">Nombre del producto</span>
+                            <span class="precio">$00.00</span>
+                        </div>
+                        <div class="producto">
+                            <span class="cantidad">1x</span>
+                            <span class="nombre_producto">Nombre del producto</span>
+                            <span class="precio">$00.00</span>
+                        </div>
+                        <div class="producto">
+                            <span class="cantidad">1x</span>
+                            <span class="nombre_producto">Nombre del producto</span>
+                            <span class="precio">$00.00</span>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="pedido_direccion">
+                        <span class="departamento">Departamento</span>
+                        <span class="nombre_direccion">Nombre direccion</span>
+                        <div class="info_direccion tooltip"><i class="fas fa-info-circle"></i>
+                            <span class="tooltiptext">Detalles de la direccion de envio para este pedido</span>
+                        </div>
+                    </div>
+                    <div class="pedido_total">
+                        <span>$00.00</span>
+                    </div>
+                </article>
+            </div>
+            <div class="button">
+                <a href="#">Ver todos</a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>

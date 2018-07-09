0fc7ac205664292ee6f8a6f3aeac021bc95b0e71
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-07-08 22:11:21 -0600

    pago
    -- Ordenar comentarios de php mail

--- pago.php
+++ pago.php
@@ -71,40 +71,40 @@ if ($conexion != false) {
 		}
 
 		//Enviar email de confirmacion de pedido
-		$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+		$mail = new PHPMailer(true);                              		// Passing `true` enables exceptions
 		
 		try {
 		    //Server settings
-		    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-		    // $mail->isSMTP();                                      // Set mailer to use SMTP
-		    $mail->Host = 'mail.arteataco.dx.am';  // Specify main and backup SMTP servers
-		    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-		    $mail->Username = 'consultas@arteataco.dx.am';                 // SMTP username
-		    $mail->Password = 'arteataco10';                           // SMTP password
-		    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-		    $mail->Port = 465;                                    // TCP port to connect to
+		    $mail->SMTPDebug = 2;										// Enable verbose debug output
+		    // $mail->isSMTP();											// Set mailer to use SMTP
+		    $mail->Host = 'mail.arteataco.dx.am';  						// Specify main and backup SMTP servers
+		    $mail->SMTPAuth = true;                               		// Enable SMTP authentication
+		    $mail->Username = 'consultas@arteataco.dx.am';          	// SMTP username
+		    $mail->Password = 'arteataco10';                        	// SMTP password
+		    $mail->SMTPSecure = 'ssl';                            		// Enable TLS encryption, `ssl` also accepted
+		    $mail->Port = 465;                                    		// TCP port to connect to
 
 		    //Recipients
 		    $mail->setFrom('arte.ataco@gmail.com', 'Arte Ataco');
-		    // $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-		    $mail->addAddress($email_user, $nombre_user);               // Name is optional
+		    // $mail->addAddress('joe@example.net', 'Joe User');     	// Add a recipient
+		    $mail->addAddress($email_user, $nombre_user);            	// Name is optional
 		    $mail->addReplyTo('arte.ataco@gmail.com', 'Nuevo pedido');
 		    // $mail->addCC('cc@example.com');
 		    // $mail->addBCC('bcc@example.com');
 
 		    //Attachments
-		    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-		    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		    // $mail->addAttachment('/var/tmp/file.tar.gz');         	// Add attachments
+		    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    	// Optional name
 
 		    //Content
-		    $mail->isHTML(true);                                  // Set email format to HTML
+		    $mail->isHTML(true);                                  		// Set email format to HTML
 		    $mail->Subject = 'Su pedido ' . '#' . $codigo . 'ha sido tomado';
 		    $mail->Body    = "Puede visitar la sección <Pedidos> en <arteataco.onlinewebshop.net> para ver los detalles";
 		    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 		    $mail->send();
 		    // echo 'Message has been sent';
-		    header("Location: mensaje_sent.php");
+		    // header("Location: mensaje_sent.php");
 		} catch (Exception $e) {
 		    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 		}
